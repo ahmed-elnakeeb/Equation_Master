c6828faad59104cbb09a08c760e86cfce2eb29e5
ahmed <ahmedelnakeeb2016@outlook.com>
2019-08-01 21:10:16 +0200

    Add project files.

---
+++ Equation_Master/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Equation_Master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
